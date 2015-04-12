d1d14c791199e650ee6cc98ccd912d983c259733
Ji Ho Koo <jihokoo@gmail.com>
2015-04-12 19:12:48 -0400

    Add test response for GET /thumbs endpoint

--- test/thumbnails.js
+++ test/thumbnails.js
@@ -1,11 +1,11 @@
 var Lab = require("lab"),
   code = require("code"),
-  server = require("../");
+  server = require("../server");
 
 var lab = exports.lab = Lab.script();
 
-lab.experiment("Thumbnails", function() {
-  lab.test("Main endpoint lists our thumbnails",
+lab.experiment("GET /thumbs", function() {
+  lab.test("should reply with Content-Type text/json",
     function(done) {
       var options = {
         method: "GET",
@@ -15,10 +15,22 @@ lab.experiment("Thumbnails", function() {
       server.inject(options, function(response) {
         var result = response.result;
 
+        code.expect('Content-Type', /json/);
         code.expect(response.statusCode).to.equal(200);
         code.expect(result).to.be.instanceof(Array);
-
+        code.expect(result).to.have.length(0);
         done();
       });
     });
+
+  lab.test("should reply with a thumbnail if there is one in the DB",
+    function(done) {
+      var options = {
+        method: "GET",
+        url: "/thumbs"
+      };
+
+      done();
+    });
 });
+

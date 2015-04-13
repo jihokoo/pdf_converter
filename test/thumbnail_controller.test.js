4a5e0e2d9a203726e83a56b67262801df113f5f7
Ji Ho Koo <jihokoo@gmail.com>
2015-04-12 23:21:54 -0400

    Edit thumbnail_controller test structure

--- test/thumbnail_controller.test.js
+++ test/thumbnail_controller.test.js
@@ -7,86 +7,89 @@ var lab = exports.lab = Lab.script();
 
 var Thumbnail = mongoose.model("Thumbnail");
 
-lab.experiment("GET /thumbs", function() {
+lab.experiment("Thumbnails Controller", function() {
 
   lab.before(function(done) {
     Thumbnail.collection.remove(done);
   });
 
-  lab.test("should reply with Content-Type text/json",
-    function(done) {
-      var options = {
-        method: "GET",
-        url: "/thumbs"
-      };
+  lab.experiment("GET /thumbs", function() {
 
-      server.inject(options, function(response) {
-        var result = response.result;
+    lab.test("should reply with Content-Type text/json",
+      function(done) {
+        var options = {
+          method: "GET",
+          url: "/thumbs"
+        };
 
-        code.expect('Content-Type', /json/);
-        code.expect(response.statusCode).to.equal(200);
-        code.expect(result).to.be.instanceof(Array);
-        code.expect(result).to.have.length(0);
-        done();
+        server.inject(options, function(response) {
+          var result = response.result;
+
+          code.expect('Content-Type', /json/);
+          code.expect(response.statusCode).to.equal(200);
+          code.expect(result).to.be.instanceof(Array);
+          code.expect(result).to.have.length(0);
+          done();
+        });
+      });
+
+    lab.test("should reply with a thumbnail if there is one in the DB", function(done) {
+      var thumbnailOne = new Thumbnail({
+        name: "LobLogo_thumb_1",
+        url: "https://s3-us-west-1.amazonaws.com/jihokoo-miscellaneous/Thumbnails/LobLogo_thumb_1.png",
+        originalFileURL: "https://s3-us-west-1.amazonaws.com/jihokoo-miscellaneous/Files/LobLogo.pdf"
+      });
+
+      thumbnailOne.save(function(err) {
+        var options = {
+          method: "GET",
+          url: "/thumbs"
+        };
+
+        server.inject(options, function(response) {
+          var result = response.result;
+
+          code.expect('Content-Type', /json/);
+          code.expect(response.statusCode).to.equal(200);
+          code.expect(result).to.be.instanceof(Array);
+          code.expect(result).to.have.length(1);
+
+          code.expect(result[0].name).to.equal(thumbnailOne.name);
+          code.expect(result[0].url).to.equal(thumbnailOne.url);
+          done();
+        });
+
       });
     });
 
-  lab.test("should reply with a thumbnail if there is one in the DB", function(done) {
-    var thumbnailOne = new Thumbnail({
-      name: "LobLogo_thumb_1",
-      url: "https://s3-us-west-1.amazonaws.com/jihokoo-miscellaneous/Thumbnails/LobLogo_thumb_1.png",
-      originalFileURL: "https://s3-us-west-1.amazonaws.com/jihokoo-miscellaneous/Files/LobLogo.pdf"
-    });
-
-    thumbnailOne.save(function(err) {
-      var options = {
-        method: "GET",
-        url: "/thumbs"
-      };
-
-      server.inject(options, function(response) {
-        var result = response.result;
-
-        code.expect('Content-Type', /json/);
-        code.expect(response.statusCode).to.equal(200);
-        code.expect(result).to.be.instanceof(Array);
-        code.expect(result).to.have.length(1);
-
-        code.expect(result[0].name).to.equal(thumbnailOne.name);
-        code.expect(result[0].url).to.equal(thumbnailOne.url);
-        done();
+    lab.test("should be able to reply with multiple thumbnails", function(done) {
+      var thumbnailTwo = new Thumbnail({
+        name: "LobLogo_thumb_2",
+        url: "https://s3-us-west-1.amazonaws.com/jihokoo-miscellaneous/Thumbnails/LobLogo_thumb_2.png",
+        originalFileURL: "https://s3-us-west-1.amazonaws.com/jihokoo-miscellaneous/Files/LobLogo.pdf"
       });
 
-    });
-  });
+      thumbnailTwo.save(function(err) {
+        var options = {
+          method: "GET",
+          url: "/thumbs"
+        };
 
-  lab.test("should be able to reply with multiple thumbnails", function(done) {
-    var thumbnailTwo = new Thumbnail({
-      name: "LobLogo_thumb_2",
-      url: "https://s3-us-west-1.amazonaws.com/jihokoo-miscellaneous/Thumbnails/LobLogo_thumb_2.png",
-      originalFileURL: "https://s3-us-west-1.amazonaws.com/jihokoo-miscellaneous/Files/LobLogo.pdf"
-    });
+        server.inject(options, function(response) {
+          var result = response.result;
 
-    thumbnailTwo.save(function(err) {
-      var options = {
-        method: "GET",
-        url: "/thumbs"
-      };
+          code.expect('Content-Type', /json/);
+          code.expect(response.statusCode).to.equal(200);
+          code.expect(result).to.be.instanceof(Array);
+          code.expect(result).to.have.length(2);
 
-      server.inject(options, function(response) {
-        var result = response.result;
+          code.expect(result[0].name).to.equal("LobLogo_thumb_1");
+          code.expect(result[1].name).to.equal(thumbnailTwo.name);
+          code.expect(result[1].url).to.equal(thumbnailTwo.url);
+          done();
+        });
 
-        code.expect('Content-Type', /json/);
-        code.expect(response.statusCode).to.equal(200);
-        code.expect(result).to.be.instanceof(Array);
-        code.expect(result).to.have.length(2);
-
-        code.expect(result[0].name).to.equal("LobLogo_thumb_1");
-        code.expect(result[1].name).to.equal(thumbnailTwo.name);
-        code.expect(result[1].url).to.equal(thumbnailTwo.url);
-        done();
       });
-
     });
   });
 });

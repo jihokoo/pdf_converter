b07c44af09d6c3dc4dd30f68faaa99ef8de06b46
Ji Ho Koo <jihokoo@gmail.com>
2015-04-14 15:55:16 -0400

    Add model tests

--- test/thumbnail_model.test.js
+++ test/thumbnail_model.test.js
@@ -12,24 +12,32 @@ lab.experiment('Thumbnail', function() {
     Thumbnail.collection.remove(done);
   });
 
-  lab.test('should have name and url fields of String', function(done) {
+  lab.test('should have name, url, and imageUrl fields of String', function(done) {
     var validThumbnail = new Thumbnail({
-      name: "LobLogo_thumb_1",
+      name: "LobLogo_thumb_1.png",
       url: "http://localhost:8000/thumbs/LobLogo_thumb_1.png",
       imageUrl: "https://s3-us-west-1.amazonaws.com/jihokoo-miscellaneous/Thumbnails/LobLogo_thumb_1.png"
     });
 
     validThumbnail.save(function(err, thumbnail) {
-      code.expect(thumbnail.name).to.equal("LobLogo_thumb_1");
+      code.expect(thumbnail.name).to.equal("LobLogo_thumb_1.png");
       code.expect(thumbnail.url).to.equal("http://localhost:8000/thumbs/LobLogo_thumb_1.png");
+      done();
+    });
+  });
 
+  lab.test('should hide imageUrl field on select', function(done) {
+    Thumbnail.findOne({name: "LobLogo_thumb_1.png"}, function(err, thumbnail) {
+      code.expect(thumbnail.name).to.equal("LobLogo_thumb_1.png");
+      code.expect(thumbnail.imageUrl).to.equal(undefined);
       done();
     });
   });
 
   lab.test("should require name", function(done) {
     var invalidThumbnail = new Thumbnail({
-      url: "https://s3-us-west-1.amazonaws.com/jihokoo-miscellaneous/Thumbnails/LobLogo_thumb_1.png"
+      url: "http://localhost:8000/thumbs/LobLogo_thumb_2.png",
+      imageUrl: "https://s3-us-west-1.amazonaws.com/jihokoo-miscellaneous/Thumbnails/LobLogo_thumb_2.png"
     });
 
     invalidThumbnail.save(function(err) {
@@ -41,7 +49,21 @@ lab.experiment('Thumbnail', function() {
 
   lab.test("should require url", function(done) {
     var invalidThumbnail = new Thumbnail({
-      name: "LobLogo_thumb_1"
+      name: "LobLogo_thumb_2.png",
+      imageUrl: "https://s3-us-west-1.amazonaws.com/jihokoo-miscellaneous/Thumbnails/LobLogo_thumb_2.png"
+    });
+
+    invalidThumbnail.save(function(err) {
+      code.expect(err.message).to.equal("Thumbnail validation failed");
+
+      done();
+    });
+  });
+
+  lab.test("should require imageUrl", function(done) {
+    var invalidThumbnail = new Thumbnail({
+      url: "http://localhost:8000/thumbs/LobLogo_thumb_2.png",
+      imageUrl: "https://s3-us-west-1.amazonaws.com/jihokoo-miscellaneous/Thumbnails/LobLogo_thumb_2.png"
     });
 
     invalidThumbnail.save(function(err) {
@@ -52,9 +74,9 @@ lab.experiment('Thumbnail', function() {
   });
 
   lab.test("should have an instance method to get itself as JSON", function(done) {
-    Thumbnail.findOne({name: "LobLogo_thumb_1"}, function(err, thumbnail) {
+    Thumbnail.findOne({name: "LobLogo_thumb_1.png"}, function(err, thumbnail) {
       var jsonThumbnail = thumbnail.asJSON();
-      code.expect(jsonThumbnail).to.match(/"name":"LobLogo_thumb_1"/);
+      code.expect(jsonThumbnail).to.match(/"name":"LobLogo_thumb_1.png"/);
 
       done();
     });

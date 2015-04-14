27ce3d3e9ebaf6132002ef17a53e4d9c4e2e878b
Ji Ho Koo <jihokoo@gmail.com>
2015-04-14 15:59:42 -0400

    Add comment

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
+/*
+  I separated index.js into two files, this file and server.js
+  to allow other files to require the server through server.js
+  without starting it
+*/
 var server = require('./server');
 
 server.start(function() {
